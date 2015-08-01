ce61a005b62a2e156731fafa66a0755010c69ad5
Kentaro Soya <ca@conquestarrow.com>
2015-08-01 16:54:45 +0900

    add sinonjs & d.ts

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -5,3 +5,4 @@
 /// <reference path="empower/empower.d.ts" />
 /// <reference path="power-assert-formatter/power-assert-formatter.d.ts" />
 /// <reference path="power-assert/power-assert.d.ts" />
+/// <reference path="sinon/sinon.d.ts" />
