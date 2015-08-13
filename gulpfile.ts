dbeb503751bf9ec648d394d56a7ff620463be3d4
Kentaro Soya <ca@conquestarrow.com>
2015-08-13 20:30:04 +0900

    add gulpfile & add task "cmdbuild" to build node/bin file

---
+++ gulpfile.ts
@@ -0,0 +1,25 @@
+/// <reference path="./typings/tsd.d.ts" />
+
+
+import gulp = require("gulp");
+import gts = require("gulp-typescript");
+import concat = require("gulp-concat");
+//var sq = require("streamqueue");
+var header = require('gulp-header');
+
+
+gulp.task("cmdbuild",()=>{
+	return 
+		gulp
+			.src(["./src/index.ts"])
+			.pipe(gts({
+				noImplicitAny:true,
+				suppressImplicitAnyIndexErrors: true,
+				module:"commonjs",
+				target:"ES5"
+			}))
+			.js
+		.pipe(header("#!/usr/bin/env node\n"))
+		.pipe(concat("dtsgen"))
+		.pipe(gulp.dest("./bin/"))
+});
