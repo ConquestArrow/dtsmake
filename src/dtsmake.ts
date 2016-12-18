66d809a1b1800aa8cc31916a6842d1bf573ccc89
ConquestArrow <ca@conquestarrow.com>
2016-12-18 23:40:59 +0900

    remove ref tag

--- src/dtsmake.ts
+++ src/dtsmake.ts
@@ -1,6 +1,3 @@
-/// <reference path="../typings/node/node.d.ts" />
-/// <reference path="../typings/clone/clone.d.ts" />
-
 import fs = require('fs');
 import path = require('path');
 import clone = require('clone');
